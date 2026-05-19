87ff1f1c8a04cded44f43308d96defbd8db7eefa
roiover <roiover@proton.me>
2026-05-20 00:53:33 +0300

    Update UploadService.js

--- src/UploadService.js
+++ src/UploadService.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PinataSDK } from "pinata";
